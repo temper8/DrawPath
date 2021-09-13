e11409e9ec41bcbc70d23fb7aed772130f655b90
Alex <temper8@gmail.com>
2021-09-13 21:50:52 +0300

    def read_bounds()

---
+++ bounds.py
@@ -0,0 +1,15 @@
+import os
+def read_bounds():
+    Icms_path = os.path.abspath("data\\lcms.dat")
+    file = open(Icms_path)
+
+    header = file.readline().split()
+    print(header)
+    lines = file.readlines()
+
+    table = [line.split() for line in lines]
+    table = list(filter(None, table))
+
+    R = [float(row[0]) for row in table]
+    Z = [float(row[1]) for row in table]
+    return R, Z
