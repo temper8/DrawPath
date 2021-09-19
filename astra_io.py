6f643977cb449c0056a90cbae5afc8860240557c
Alex <temper8@gmail.com>
2021-09-20 02:18:37 +0300

    read_trajectories2

--- astra_io.py
+++ astra_io.py
@@ -39,3 +39,35 @@ def read_trajectories(f):
         ray.Z.append(float(row[1]))
 
     return rays, N_traj
+
+def read_trajectories2(f):
+    Icms_path = os.path.abspath(f)
+    file = open(Icms_path)
+
+    header = file.readline().replace('=', '_').split()
+
+    print(header)
+
+    empty_ray = dict([ (h, []) for h in header ])
+
+    lines = file.readlines()
+    table = [line.split() for line in lines]
+    table = list(filter(None, table))
+
+    rays = []
+    N_traj = 0
+    #header.append('N_traj')
+    TRay = namedtuple('Ray' , header)
+
+    for row in table:
+        if N_traj != int(row[12]):
+            N_traj = int(row[12])
+            ray = dict([ (h, []) for h in header ])
+            rays.append(ray)
+        for index, (p, item) in enumerate(ray.items()):
+            item.append(float(row[index]))
+        #    print(p, item, index)
+        # break
+
+
+    return rays, N_traj
